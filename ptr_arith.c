9b75ae97c4ec765a4de8ef0de5e77423d209b5ca
Chris Geroux <chris.geroux@ace-net.ca>
2021-03-02 15:42:05 +0000

    Removed malloc from ptr_arth.c

--- ptr_arith.c
+++ ptr_arith.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 int main(){
-  void* p=malloc(1000*sizeof(int));
+  int array[10];
+  void* p=&array[0];
   printf("p=%p\n",p);
   printf("(void*)p+1=%p\n",p+1);
-  printf("(int*)p+1=%p\n",(void*)( (int*)p +1 ));
+  printf("(int*)p+1=%p\n",(int*)p+1 );
 }
