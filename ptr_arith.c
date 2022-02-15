cd3e1bc810de87c805078e1e3c11b6df3fe4671c
Ross Dickson <ross.dickson@ace-net.ca>
2022-02-15 21:38:07 +0000

    Clarify and expand ptr_arith example

--- ptr_arith.c
+++ ptr_arith.c
@@ -3,7 +3,8 @@
 int main(){
   int array[10];
   void* p=&array[0];
-  printf("p=%p\n",p);
-  printf("(void*)p+1=%p\n",p+1);
-  printf("(int*)p+1=%p\n",(int*)p+1 );
+  printf("         p     = %p\n",p);
+  printf("  (void*)p + 1 = %p\n",p+1);
+  printf("   (int*)p + 1 = %p\n",(int*)p+1);
+  printf("(double*)p + 1 = %p\n",(double*)p+1);
 }
