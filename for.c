b6a41af30a6ccdfcd89aef0bdca96e1442a9cd9a
Ross Dickson <ross.dickson@ace-net.ca>
2026-06-05 14:11:22 -0300

    Solution to for loop quiz

---
+++ for.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+   int i;
+   for ( i = 1; i < 5; i++ ) printf("%d ",i);
+   printf("\n");
+   for ( i = 0; i < 5; i++ ) printf("%d ",i);
+   printf("\n");
+   for ( i = 0; i < 6; ++i ) printf("%d ",i);
+   printf("\n");
+}
