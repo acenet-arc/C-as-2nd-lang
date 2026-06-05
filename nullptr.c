1067724b3a3c2b71b32ed7b8c8eb3bcf2f431d99
Ross Dickson <ross.dickson@ace-net.ca>
2026-06-05 15:49:13 -0300

    elaborate nullptr.c

--- nullptr.c
+++ nullptr.c
@@ -3,7 +3,11 @@
 int main()
 {
     int *ip;
+    int i = 1234;
+    ip = &i;
+    printf("Valid address: %p\n", ip);
     ip = NULL;
-    printf("Points to:     %x\n", ip);
-    printf("Null pointer:  %d\n", *ip);
+    printf("NULL address:  %p\n", ip);
+    printf("Attempt to dereference follows...\n");
+    printf("%d\n", *ip);
 }
